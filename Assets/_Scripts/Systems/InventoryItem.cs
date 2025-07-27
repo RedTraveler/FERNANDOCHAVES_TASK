9236d53c6facde87ec59d5db33fdeb56ae465427
RED <79979318+RedTraveler@users.noreply.github.com>
2025-07-27 06:41:52 -0300

    Inventory Drag & Drop basic
    Implemented the basic of the Inventory system.

---
+++ Assets/_Scripts/Systems/InventoryItem.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [Header("UI")]
+    public Image image;
+
+    [HideInInspector] public Transform parentAfterDrag;
+
+    //Class for drag and drop items
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = false;
+        parentAfterDrag = transform.parent;
+        transform.SetParent(transform.root);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        image.raycastTarget = true;
+        transform.SetParent(parentAfterDrag);
+    }
+
+}
